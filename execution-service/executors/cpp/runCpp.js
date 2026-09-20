f3a7609056211b1a963a648be268f2588732d4de
Abhinash <abhimishrabhi2580@gmail.com>
2026-09-20 18:00:16 +0530

    added file for c++ processor

--- execution-service/executors/cpp/runCpp.js
+++ execution-service/executors/cpp/runCpp.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const { runProcess } = require('../../utils/runProcess');
 
-const runCpp = async (exePath, input, timeLimit) => {
+const runCpp = async (exePath, inputPath, timeLimit) => {
+
+    const input = fs.readFileSync(inputPath, 'utf-8');
 
     return await runProcess(exePath, [], input, timeLimit);
 };
